0ab0e40497bdeeec692d6ac2e1838bf9f9984a35
Farrukh Ahmed Khan <khanfarrukh200@gmail.com>
2021-07-06 17:19:55 +0500

    covid tracker with redux

---
+++ src/components/footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+let Footr = () => {
+    return (
+        <div style={{
+            backgroundColor:"#4b4b4b",
+            padding:"25px",
+            color:"whitesmoke"
+        }}>
+            <footer>
+                <div>
+                    <h3><b>Developer : Farrukh Ahmed Khan</b></h3>
+                    <h3><b>github : Farrukh-cyber</b></h3>
+                </div>
+            </footer>
+
+        </div>
+    )
+}
+export default Footr;
